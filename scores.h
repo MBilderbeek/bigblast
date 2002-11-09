6755ca3837529d0457b95a063190e370b857b107
manuel <manuel>
2002-11-09 00:22:58 +0000

    Added scores, shield-o-meter and fixed bg-bug

---
+++ scores.h
@@ -0,0 +1,21 @@
+/* $\Id$
+ *
+ * AUTHOR(S)   : M. Bilderbeek & E. Boon
+ *
+ * DESCRIPTION :
+ *   contains definitions of the scores. That's all folks!
+ */
+
+extern unsigned int score;
+
+#define SC_AST_SMALL 20
+#define SC_AST_MEDIUM 15
+#define SC_AST_BIG 10
+
+#define SC_LEVEL(l) (100*l)
+
+#define TIME_BONUS 1000
+
+//#define BULLET_BONUS(l) Mindah schietah is betah!
+
+
